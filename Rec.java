0898a2ee4b1f0ff283663bbb6a89ca3d93b33a83
JirapinyaInfaktha <yoye_foreverkiki@hotmail.com>
2016-09-03 11:44:18 +0700

    Update Rec.java

--- Rec.java
+++ Rec.java
@@ -8,7 +8,8 @@ public class Rec {
     public static int numDigits(int n) {
 	
 	// fill me in
-	
+	if((n<10) && (n>-10)) return 1;
+	return 1 + numDigits(n/10);
     }
 
     /** = sum of the digits in the decimal representation of n.
